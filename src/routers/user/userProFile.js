f5e8f5504c65daa3fafca73da196c6ede2833c5c
jmgucong <jm_gcc@163.com>
2021-08-18 15:16:38 +0800

    优化头像无法显示的问题

--- src/routers/user/userProFile.js
+++ src/routers/user/userProFile.js
@@ -1,25 +1,23 @@
 import { Card, Avatar } from "antd";
 import { useSelector } from "react-redux";
 import { UserOutlined } from "@ant-design/icons";
-import { useHistory, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import { useLoadUserProfile } from "../../store/action/user";
 
 function UserProfile(props) {
     const userId = useLocation().pathname.split('/')[2];
-    const { replace } = useHistory();
     const { profile = {} } = useSelector((state) => state.user);
-    const { avatar } = useSelector(state=>state.userInfo);
     const getUserProfile = useLoadUserProfile();
     useEffect(() => {
         getUserProfile(userId);
 
     }, [])
-
+    let avatatHttp = 'http://39.99.151.246/';
     return <div>
         {profile.username ? <Card type="inner" title={<a href="/">主页/</a>}>
-            <a href={'/user/'+profile.userId} onClick={() => replace('/user')}>
-                <Avatar src={profile.avatar} icon={<UserOutlined />} />
+            <a href={'/user/'+profile.id}>
+                <Avatar src={profile.avatar?profile.avatar.indexOf('https') !== -1?profile.avatar:avatatHttp+profile.avatar:''} icon={<UserOutlined />} />
             </a>
             <div>姓名：{profile.username}</div>
         </Card> : ''}
